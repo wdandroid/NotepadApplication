a602182554a29cdcdd61eeb9faa2df0a8e0efb2d
wdandroid <wdandroid@163.com>
2016-04-22 11:39:51 +0800

    change to api 21

--- app/src/main/java/com/cskaoyan/notepad/notepadapplication/MainActivity.java
+++ app/src/main/java/com/cskaoyan/notepad/notepadapplication/MainActivity.java
@@ -1,9 +1,9 @@
 package com.cskaoyan.notepad.notepadapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
